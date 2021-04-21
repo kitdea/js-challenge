c7ca25b8b2a4e611c768cb2d3d9a222b71e1d7ac
kitdea <francismarrosales@gmail.com>
2021-04-21 20:32:03 +0800

    Challenge 3 not yet done. Added some css and script

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Age in Days
+// Challenge 1: Age in Days
 
 function stoneAge() {
   var birthYear = prompt('What is your Year of Birth?');
@@ -15,15 +15,37 @@ function reset() {
   document.getElementById('stoneAge').remove();
 }
 
+// Challenge 2: Generator
+
 function generate() {
   var img = document.createElement('img');
   var div = document.getElementById('flexbox-img');
   img.src = "https://media.giphy.com/media/Oivne3ir8TzcA/giphy.gif";
+  // img.src = "https://media.giphy.com/media/1pOprEJ24zcM8/giphy.gif";
   img.setAttribute('id', 'generate');
   div.appendChild(img);
 }
 
-
 function resetImg() {
   document.getElementById('generate').remove();
+}
+
+// Challenge 3: Rock, Paper, Scissor
+
+function rpsGame(yourChoice) {
+  console.log(yourChoice);
+  var humanChoice, botChoice;
+  // humanChoice = yourChoice.id;
+  // botChoice = yourChoice.id;
+  // results = decideWinner(humanChoice, botChoice); //[0, 1] human lost | bot won
+  // message = finalMessage(results); // {'message': 'You Won', 'color': 'green'}
+  rpsFrontEnd(yourChoice.id, botChoice, message);
+}
+
+function randToRpsInt() {
+  return Math.floor(Math.random() * 3);
+}
+
+function numberToChoice(number) {
+  return ['rock', 'paper', 'scissor'] [number]
 }
